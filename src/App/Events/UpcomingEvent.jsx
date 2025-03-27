0a5c52171b86cad8e5f4baa5bd6b4647385576c8
Ahmed Amer <amer73090@gmail.com>
2025-03-27 21:36:25 +0200

    update-slide

--- src/App/Events/UpcomingEvent.jsx
+++ src/App/Events/UpcomingEvent.jsx
@@ -16,8 +16,8 @@ const UpcomingEvent = () => {
                 Upcoming <span className="text-blue-700">Event</span>
             </h1>
             <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 mb-[10px]">
-                <img src={post} alt="Upcoming Event" className="mx-auto rounded-xl w-[80%]" />
-                <div className="details ml-[10px] ">
+                <img src={post} alt="Upcoming Event" className="mx-auto mb-2 rounded-xl w-[80%]" />
+                <div className="details   w-[80%] mx-auto ">
                     <h2 className="mt-5  text-center">{event.date} - {event.title}</h2>
                     <h1 className="mt-5  font-bold  text-center">{event.mainTitle}</h1>
                     <p className="mt-5 text-center">{event.description}</p>
--- src/App/Landing/Landing.jsx
+++ src/App/Landing/Landing.jsx
@@ -37,13 +37,13 @@ const Landing = () => {
                 </div>
             </section> */}
             {/* section bg-image */}
-            <section className="home-page  h-[90vh]  ">
-                <div className="bg-[url('/images/bg-image-team2025-1.jpg')]  bg-cover bg-center h-[90%] w-[100%] relative ">
-                    <div className="container relative  top-[15%] left-[7%] font-bold ">
-                        <h1 className="title  text-[25px] lg:text-[40px]   md:text-[30px]  ">
+            <section className="home-page h-[90vh]  ">
+                <div className="bg-[url('/images/bg-image-team2025-1.jpg')] bg-cover bg-center h-[90%] w-full relative  ">
+                    <div className="container relative  top-[15%] left-[7%] w-fit text font-bold ">
+                        <h1 className="title  text-[25px]   md:text-[30px]  lg:text-[40px] ">
                             Welcome to  <span className="text-primary"> IEEE OCS SB</span>
                         </h1>
-                        <p className="mt-2 text-[15px] lg:text-[18px]  md:text-[12px]">
+                        <p className="mt-2 text-[10px]  md:text-[12px] lg:text-[18px]">
                             Take a tour of work management platform to <br />{" "}
                             see how you can do more of your best work.
                         </p>
